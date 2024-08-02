ca342d200984c25d4164a3effc866e6611882f7b
Bogdan Isaev <redzor@live.com>
2024-08-02 11:41:57 +0300

    initial

--- src/main/resources/sql/component_stock_first.sql
+++
@@ -1,4 +0,0 @@
-select *,
-       (select count(*) from component_stock_codes where code like csc.code || '%' and length(code) = length(csc.code) + 3 and removed = 0) as child_count
-from component_stock_codes csc where id = &id
-
